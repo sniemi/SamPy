92a356a54a080775e39cba63f6f109b5bf55f964
Sami Niemi <niemi@stsci.edu>
2011-03-15 20:40:19 -0400

    Small changes to one of the Herschel scripts.

--- herschel/calculateMergers.py
+++ herschel/calculateMergers.py
@@ -14,20 +14,56 @@ if __name__ == '__main__':
 
     mergetimelimit = 0.25
 
-    print 'Calculating merger statistics from:'
-    print path + db
-    print 'with mergetimelimit =', mergetimelimit
+#    print 'Calculating merger statistics from:'
+#    print path + db
+#    print 'with mergetimelimit =', mergetimelimit
+#
+#    query2 = '''select galprop.tmerge, galprop.tmajmerge
+#                from FIR, galprop where
+#                galprop.mstar > 10.0 and
+#                FIR.z >= 2.0 and
+#                FIR.z < 4.0 and
+#                FIR.spire250_obs < 1e6 and
+#                FIR.gal_id = galprop.gal_id and
+#                FIR.halo_id = galprop.halo_id
+#                '''
+#    #get data, massive galaxies
+#    data = sq.get_data_sqliteSMNfunctions(path, db, query2)
+#    tmerge = data[:,0]
+#    tmajor = data[:,1]
+#    #masks
+#    nomergeMask = tmerge < 0.0
+#    majorsMask = (tmajor > 0.0) & (tmajor <= mergetimelimit)
+#    majorsMask2 = (tmajor > mergetimelimit)
+#    mergersMask = (tmerge > 0.0) & (tmerge <= mergetimelimit) & \
+#                  (majorsMask == False) & (majorsMask2 == False)
+#    mergersMask2 = (nomergeMask == False) & (majorsMask == False) & \
+#                   (mergersMask == False) & (majorsMask2 == False)
+#    #the fraction of no mergers?
+#    nm2 = len(tmerge[tmerge < 0.0]) / float(len(tmerge)) * 100.
+#    nm3 = len(tmajor[tmajor < 0.0]) / float(len(tmajor)) * 100.
+#    nm4 = len(tmajor[majorsMask]) / float(len(tmajor)) * 100.
+#    #print out some statistics
+#    print 'Number of galaxies and Poisson error:', len(tmerge), N.sqrt(len(tmerge))
+#    print 'Mean tmerge of M_star > 10**10 galaxies', N.mean(tmerge[tmerge > 0.0])
+#    print 'Max tmerge of M_star > 10**10 galaxies', N.max(tmerge[tmerge > 0.0])
+#    print 'Fraction of M_star > 10**10 have experienced a merger', 100.-nm2
+#    print 'Fraction of M_star > 10**10 have experienced a major merger', 100.-nm3
+#    print 'Fraction of M_star > 10**10 who have experienced their major merger within mergetimlimit', nm4
+#    print
 
+###############################################################################
+        
     query2 = '''select galprop.tmerge, galprop.tmajmerge
                 from FIR, galprop where
-                galprop.mstar > 10.0 and
                 FIR.z >= 2.0 and
                 FIR.z < 4.0 and
                 FIR.spire250_obs < 1e6 and
+                FIR.spire250_obs > 1e-40 and
                 FIR.gal_id = galprop.gal_id and
                 FIR.halo_id = galprop.halo_id
                 '''
-    #get data, massive galaxies
+    #get data
     data = sq.get_data_sqliteSMNfunctions(path, db, query2)
     tmerge = data[:,0]
     tmajor = data[:,1]
@@ -45,15 +81,16 @@ if __name__ == '__main__':
     nm4 = len(tmajor[majorsMask]) / float(len(tmajor)) * 100.
     #print out some statistics
     print 'Number of galaxies and Poisson error:', len(tmerge), N.sqrt(len(tmerge))
-    print 'Mean tmerge of M_star > 10**10 galaxies', N.mean(tmerge[tmerge > 0.0])
-    print 'Max tmerge of M_star > 10**10 galaxies', N.max(tmerge[tmerge > 0.0])
-    print 'Fraction of M_star > 10**10 have experienced a merger', 100.-nm2
-    print 'Fraction of M_star > 10**10 have experienced a major merger', 100.-nm3
-    print 'Fraction of M_star > 10**10 who have experienced their major merger within mergetimlimit', nm4
+    print 'Mean tmerge of all galaxies', N.mean(tmerge[tmerge > 0.0])
+    print 'Max tmerge of all galaxies', N.max(tmerge[tmerge > 0.0])
+    print 'Fraction of all galaxies that have experienced a merger', 100.-nm2
+    print 'Fraction of all galaxies that have experienced a major merger', 100.-nm3
+    print 'Fraction of all galaxies that who have experienced their major merger within mergetimlimit', nm4
     print
+    
+    import sys; sys.exit()
 
 ###############################################################################
-    import sys; sys.exit()
         
     query2 = '''select galprop.tmerge, galprop.tmajmerge
                 from FIR, galprop where
